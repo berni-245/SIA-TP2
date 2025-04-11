98b58ba21a42b1784686f7e13401a995df9e1329
berni <bernizapico@gmail.com>
2025-04-10 23:36:04 -0300

    feat: added universal and roulette selection technique

--- src/main.py
+++ src/main.py
@@ -2,9 +2,12 @@ from PIL import Image
 
 from generator import Generator, ShapeType
 
-reference_img = Image.open("./assets/triangles.png").convert("RGBA")
+reference_img = Image.open("./assets/franco.png").convert("RGBA")
 
-gen = Generator(reference_img, 30, ShapeType.ELLIPSE, 5)
+gen = Generator(reference_img, 5, ShapeType.TRIANGLE, 5)
+
+for _ in range(4):
+    gen.trad_generational_jump()
 
 for i, indi in enumerate(gen.individuals):
     indi.img.save(f"./generated/test-ellipse-{i:02}.png")
